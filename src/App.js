141f9266481abcacb17e4a7184ee807db59d1b52
spegisima <agneta.smergelyte@gmail.com>
2023-11-18 15:10:37 +0200

    redirection delay

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ const voice = new Audio(revelationSound);
 function App() {
     const [revealed, setRevealed] = useState('');
     const [words, setWords] = useState('Are you ready for the truth?');
+    const [animation, setAnimation] = useState(true);
     function revealTruth() {
-        if (revealed === '') {
+        if (animation) {
             voice.play();
             setRevealed('reveal');
             setTimeout(() => {
                 setWords('You just found the best web developer for the job');
+                setTimeout(() => {
+                    setAnimation(false)
+                }, 3500)
             }, 3500);
         } else {
             window.location.href = 'https://www.agnetasmergelyte.lt/';
